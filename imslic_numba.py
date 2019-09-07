a8183845f6cbf147553968768829d673f3a18b1d
AlessandroToschi <ialessandrotoschi@gmail.com>
2019-09-07 14:27:15 +0200

    djkstra via heap

--- imslic_numba.py
+++ imslic_numba.py
@@ -4,6 +4,7 @@ from numba import njit, prange, stencil
 from numba import types
 from numba.typed import Dict
 import time
+import heapq
 from rgb2lab import rgb_to_lab
 from area import compute_area, compute_cumulative_area
 from seeds import compute_seeds, unravel_index
@@ -29,48 +30,68 @@ def compute_lambda_factor(seed_position, region_size, area, xi, height, width):
     sub_region_area = area[y_min : y_max + 1, x_min : x_max + 1].sum()
     return np.sqrt(xi / sub_region_area)
 
+@njit(cache=True)
+def sp_d(seed_position, offset, lab_image):
+    x_min = int(max(0, seed_position[1] - offset))
+    x_max = int(min(lab_image.shape[1] - 1, seed_position[1] + offset))
+    y_min = int(max(0, seed_position[0] - offset))
+    y_max = int(min(lab_image.shape[0] - 1, seed_position[0] + offset))
+    delta_x = (x_max - x_min) + 1
+    delta_y = (y_max - y_min) + 1
+    distances = np.ones((delta_x * delta_y)) * 1E12
+    heap = []
+    for y in range(y_min, y_max + 1):
+        for x in range(x_min, x_max + 1):
+            if y == seed_position[0] and x == seed_position[1]:
+                heap.append((0.0, y, x))
+                distances[(y - y_min) * delta_x + x - x_min] = 0.0
+            else:
+                heap.append((1E12, y, x))
+    heapq.heapify(heap)
+
 @njit(parallel=True)
 def compute_regions_distances(K, seeds_positions, region_size, area, xi, height, width, lab_image, sp_dict):
     for k in prange(K):
         lambda_factor = compute_lambda_factor(seeds_positions[:, k], region_size, area, xi, height, width)
         offset = region_size * lambda_factor
-        x_min = int(max(0, seeds_positions[1, k] - offset))
-        x_max = int(min(width - 1, seeds_positions[1, k] + offset))
-        y_min = int(max(0, seeds_positions[0, k] - offset))
-        y_max = int(min(height - 1, seeds_positions[0, k] + offset))
-        delta_x = (x_max - x_min) + 1
-        delta_y = (y_max - y_min) + 1
-        region_graph = np.zeros((delta_x * delta_y, delta_x * delta_y))
-        for y in range(delta_y - 1):
-            for x in range(delta_x):
-                index = y * delta_x + x
-                east_neighbor_index = index + 1
-                south_neighbor_index = index + delta_x
-                east_distance = np.sqrt(
-                    1 + np.sum(np.power(lab_image[y_min + y, x_min + x, :] - lab_image[y_min + y, x_min + x + 1, :], 2.0))
-                )
-                south_distance = np.sqrt(
-                    1 + np.sum(np.power(lab_image[y_min + y, x_min + x, :] - lab_image[y_min + y + 1, x_min + x, :], 2.0))
-                )
-                region_graph[index, east_neighbor_index] = east_distance
-                region_graph[east_neighbor_index, index] = east_distance
-                region_graph[index, south_neighbor_index] = south_distance
-                region_graph[south_neighbor_index, index] = south_distance
-        shortest_region_path = shortest_path(region_graph)
-        paths = np.zeros((4, shortest_region_path.shape[0] - 1))
-        seed_index = int((seeds_positions[0, k] - y_min) * delta_x + seeds_positions[1, k] - x_min)
-        counter = 0
-        for i in range(shortest_region_path.shape[0]):
-            if i != seed_index:
-                y_d, x_d = unravel_index(i, delta_x)
-                y, x = y_d + y_min, x_d + x_min
-                #paths[:, counter] = [y, x, shortest_region_path[i, seed_index], k]
-                paths[0, counter] = y
-                paths[1, counter] = x
-                paths[2, counter] = shortest_region_path[i, seed_index]
-                paths[3, counter] = k
-                counter += 1
-        sp_dict[k] = paths
+        sp_d(seeds_positions[:, k], offset, lab_image)
+        #x_min = int(max(0, seeds_positions[1, k] - offset))
+        #x_max = int(min(width - 1, seeds_positions[1, k] + offset))
+        #y_min = int(max(0, seeds_positions[0, k] - offset))
+        #y_max = int(min(height - 1, seeds_positions[0, k] + offset))
+        #delta_x = (x_max - x_min) + 1
+        #delta_y = (y_max - y_min) + 1
+        #region_graph = np.zeros((delta_x * delta_y, delta_x * delta_y))
+        #for y in range(delta_y - 1):
+        #    for x in range(delta_x):
+        #        index = y * delta_x + x
+        #        east_neighbor_index = index + 1
+        #        south_neighbor_index = index + delta_x
+        #        east_distance = np.sqrt(
+        #            1 + np.sum(np.power(lab_image[y_min + y, x_min + x, :] - lab_image[y_min + y, x_min + x + 1, :], 2.0))
+        #        )
+        #        south_distance = np.sqrt(
+        #            1 + np.sum(np.power(lab_image[y_min + y, x_min + x, :] - lab_image[y_min + y + 1, x_min + x, :], 2.0))
+        #        )
+        #        region_graph[index, east_neighbor_index] = east_distance
+        #        region_graph[east_neighbor_index, index] = east_distance
+        #        region_graph[index, south_neighbor_index] = south_distance
+        #        region_graph[south_neighbor_index, index] = south_distance
+        #shortest_region_path = shortest_path(region_graph)
+        #paths = np.zeros((4, shortest_region_path.shape[0] - 1))
+        #seed_index = int((seeds_positions[0, k] - y_min) * delta_x + seeds_positions[1, k] - x_min)
+        #counter = 0
+        #for i in range(shortest_region_path.shape[0]):
+        #    if i != seed_index:
+        #        y_d, x_d = unravel_index(i, delta_x)
+        #        y, x = y_d + y_min, x_d + x_min
+        #        #paths[:, counter] = [y, x, shortest_region_path[i, seed_index], k]
+        #        paths[0, counter] = y
+        #        paths[1, counter] = x
+        #        paths[2, counter] = shortest_region_path[i, seed_index]
+        #        paths[3, counter] = k
+        #        counter += 1
+        #sp_dict[k] = paths
     print("Fine iterazione")
 
 def main():
